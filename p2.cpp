c6d7aeab7e9c359e36d523f1649264715cd5f99a
CHLLHC <chl41993@gmail.com>
2017-10-19 20:43:15 -0700

    Init

--- p2.cpp
+++ p2.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 #include "lapacke.h"
 #include "blas.h"
-#include <time.h>
+#include <ctime>
+#include <cstdio>
+
+
+
+//HDdiff is from Stackoverflow
+struct timespec HDdiff(struct timespec start, struct timespec end)
+{
+	struct timespec temp;
+	if ((end.tv_nsec - start.tv_nsec) < 0) {
+		temp.tv_sec = end.tv_sec - start.tv_sec - 1;
+		temp.tv_nsec = 1000000000 + end.tv_nsec - start.tv_nsec;
+	}
+	else {
+		temp.tv_sec = end.tv_sec - start.tv_sec;
+		temp.tv_nsec = end.tv_nsec - start.tv_nsec;
+	}
+	return temp;
+}
+
+
 
 void testLapack() {
 	struct timespec begin, end, diff;
