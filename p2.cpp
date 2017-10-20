5949939c29035454530cd7612aa27f2f62ab4926
CHLLHC <chl41993@gmail.com>
2017-10-19 22:17:17 -0700

    Init

--- p2.cpp
+++ p2.cpp
@@ -3,6 +3,8 @@
 #include "blas.h"
 #include <ctime>
 #include <cstdio>
+#include <cstdlib>
+#include <cstring>
 
 
 
@@ -23,15 +25,18 @@ struct timespec HDdiff(struct timespec start, struct timespec end)
 
 
 
-void testLapack() {
+void testLapack(double *a, double *b, int n) {
 	struct timespec begin, end, diff;
-
+	int lda = n, info = 3;
+	int *ipiv = new int[n];
+	
 	clock_gettime(CLOCK_MONOTONIC, &begin);
 
+	LAPACK_dgetrf(&n, &n, a, &lda, ipiv, &info);
 
 	clock_gettime(CLOCK_MONOTONIC, &end);
 	diff = HDdiff(begin, end);
-	printf("Blocked cache and register ijk, n=%d, B=%d, Time:%ld seconds and %ld nanoseconds.\n", n, B, diff.tv_sec, diff.tv_nsec);
+	//printf("Blocked cache and register ijk, n=%d, B=%d, Time:%ld seconds and %ld nanoseconds.\n", n, B, diff.tv_sec, diff.tv_nsec);
 }
 
 
@@ -39,6 +44,32 @@ void testLapack() {
 
 
 int main() {
+	double *a, *b;
+	int n = 10;
+	srand(419);
+	a = new double[n*n];
+	b = new double[n];
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++i) {
+			a[i*n+j] = (double)((rand() << 15) | rand()) / (double)rand();
+		}
+		b[i] = (double)((rand() << 15) | rand()) / (double)rand();
+	}
+
+	double *ai, *bi;
+	ai = new double[n*n];
+	bi = new double[n];
+	memcpy(ai, a, n*n * sizeof(double));
+	memcpy(bi, b, n * sizeof(double));
+
+
+
+
+	delete[] a;
+	delete[] b;
+	delete[] ai;
+	delete[] bi;
 
 	return 0;
 }
