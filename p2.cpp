8df6e8d63c2ba1097de66bd8403b562a569e0d2a
CHLLHC <chl41993@gmail.com>
2017-10-19 20:41:55 -0700

    Init

--- p2.cpp
+++ p2.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 #include "lapacke.h"
 #include "blas.h"
+#include <time.h>
+
+void testLapack() {
+	struct timespec begin, end, diff;
+
+	clock_gettime(CLOCK_MONOTONIC, &begin);
+
+
+	clock_gettime(CLOCK_MONOTONIC, &end);
+	diff = HDdiff(begin, end);
+	printf("Blocked cache and register ijk, n=%d, B=%d, Time:%ld seconds and %ld nanoseconds.\n", n, B, diff.tv_sec, diff.tv_nsec);
+}
+
+
+
 
 
 int main() {
+
 	return 0;
 }
