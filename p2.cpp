1dafc983c1c95b7210a8865bd2f91e6de799580d
CHLLHC <chl41993@gmail.com>
2017-10-22 16:20:48 -0700

    Init

--- p2.cpp
+++ p2.cpp
@@ -7,9 +7,11 @@
 #include <cstdlib>
 #include <cstring>
 #include <cmath>
+#include <algorithm>
 
 //declearation
 int mydgetrf(int row, int col, double *a, int lda, int *ipiv);
+int Blocked_dgetrf(int row, int col, double *a, int lda, int *ipiv, int block_size);
 int mydtrsm(char trans, int n, int nrhs, double *a, int lda, int* ipiv, double *b, int ldb);
 
 //HDdiff is from Stackoverflow
@@ -27,6 +29,7 @@ struct timespec HDdiff(struct timespec start, struct timespec end)
 	return temp;
 }
 
+//Sqeuence to time the LAPACK perfermance
 void testLapack(double *a, double *b, int n) {
 	struct timespec begin, end, diff;
 	int lda = n, info = 3;
@@ -52,6 +55,7 @@ void testLapack(double *a, double *b, int n) {
 	printf("LAPACK, n=%d, Time:%ld seconds and %ld nanoseconds.\n", n, diff.tv_sec, diff.tv_nsec);
 }
 
+//Sqeuence to time the my algorithm perfermance
 void testMine(double *a, double *b, int n) {
 	struct timespec begin, end, diff;
 	int lda = n, info = 3;
@@ -74,13 +78,37 @@ void testMine(double *a, double *b, int n) {
 
 	clock_gettime(CLOCK_MONOTONIC, &end);
 	diff = HDdiff(begin, end);
-	printf("MyFunction, n=%d, Time:%ld seconds and %ld nanoseconds.\n", n, diff.tv_sec, diff.tv_nsec);
+	printf("My algorithm, n=%d, Time:%ld seconds and %ld nanoseconds.\n", n, diff.tv_sec, diff.tv_nsec);
 }
 
+//Sqeuence to time the my GEPP algorithm perfermance
+void testBlcoked(double *a, double *b, int n) {
+	struct timespec begin, end, diff;
+	int lda = n, info = 3;
+	int *ipiv = new int[n];
 
+	clock_gettime(CLOCK_MONOTONIC, &begin);
 
+	info = Blocked_dgetrf(n, n, a, lda, ipiv);
+	if (info != 0) {
+		std::cout << "mydgetrf FAILED" << std::endl;
+		return;
+	}
+
+	char TRANS = 'N';
+	int m = 1;
+	info = mydtrsm(TRANS, n, m, a, n, ipiv, b, n);
+	if (info != 0) {
+		std::cout << "mydtrsm FAILED" << std::endl;
+	}
+
+	clock_gettime(CLOCK_MONOTONIC, &end);
+	diff = HDdiff(begin, end);
+	printf("My Blocked GEPP, n=%d, Time:%ld seconds and %ld nanoseconds.\n", n, diff.tv_sec, diff.tv_nsec);
+}
+
+//Unblocked dgetrf
 int mydgetrf(int row, int col, double *a, int lda, int *ipiv) {
-
 	int n = row;
 	if (n != col) {
 		std::cout << "ERROR, ONLY SUPPORT REGTANGLE MATRIX" << std::endl;
@@ -101,7 +129,7 @@ int mydgetrf(int row, int col, double *a, int lda, int *ipiv) {
 			return -1;
 		}
 		else {
-			//save pivoting infomation in LAPACK formate
+			//save pivoting infomation in LAPACK format
 			ipiv[i] = maxp + 1;
 			if (maxp != i) {
 				//swap rows
@@ -122,6 +150,68 @@ int mydgetrf(int row, int col, double *a, int lda, int *ipiv) {
 	return 0;
 }
 
+//Blocked dgetrf
+int Blocked_dgetrf(int row, int col, double *a, int lda, int *ipiv, int block_size) {
+	int info = 0;
+	if (row != col) {
+		std::cout << "ERROR, ONLY SUPPORT REGTANGLE MATRIX" << std::endl;
+		return -1;
+	}
+	for (int p = 0; p < row; p += block_size) {
+		int pb = std::min(row - p, block_size);
+
+		////DGETRF2
+		int rowToGo = row - p;
+		int colToGo = pb;
+		ipiv[row - 1] = row;//p+row-1=row-1
+		for (int i = p; i < row - 1; ++i) {
+			int maxp = i;
+			int max = abs(a[i*lda + i]);
+			for (int t = i + 1; t < row; ++t) {
+				if (abs(a[i*lda + t]) > max) {
+					maxp = t;
+					max = abs(a[i*lda + t]);
+				}
+			}
+			if (max == 0) {
+				std::cout << "LUfactoration failed: coefficient matrix is singular" << std::endl;
+				return -1;
+			}
+			else {
+				//save pivoting infomation in LAPACK format
+				ipiv[i] = maxp + 1;
+				if (maxp != i) {
+					//swap rows
+					for (int j = p; j < p + colToGo; ++j) {
+						double tmp = a[j*lda + i];
+						a[j*lda + i] = a[j*lda + maxp];
+						a[j*lda + maxp] = tmp;
+					}
+				}
+			}
+			for (int j = i + 1; j < row; ++j) {
+				a[i*lda + j] /= a[i*lda + i];
+				for (int k = i + 1; k < p + colToGo; k++) {
+					a[k*lda + j] -= a[i*lda + j] * a[k*lda + i];
+				}
+			}
+		}
+		////END DGETRF2
+
+
+
+
+
+
+
+
+	}
+	return 0;
+}
+
+
+
+//My dtrsm
 //signiture based on LAPACKE_dgetrs
 int mydtrsm(char trans, int n, int nrhs, double *a, int lda, int* ipiv, double *b, int ldb) {
 	if (trans != 'N') {
@@ -181,28 +271,70 @@ int main(int argc, char *argv[]) {
 	a = new double[n*n];
 	b = new double[n];
 
+	n = 9;
+	a[0] = 1;
+	a[1] = 2;
+	a[2] = 1;
+	a[3] = 2;
+	a[4] = 1;
+	a[5] = 1;
+	a[6] = 3;
+	a[7] = 1; 
+	a[8] = 1;
+	b[0] = 1;
+	b[1] = 1;
+	b[2] = 1;
+
+	/*
 	for (int i = 0; i < n; ++i) {
 		for (int j = 0; j < n; ++j) {
 			a[i*n + j] = (double)((rand() << 15) | rand()) / (double)rand();
 		}
 		b[i] = (double)((rand() << 15) | rand()) / (double)rand();
-	}
+	}*/
 
-	double *al, *bl;
+	double *al, *bl, *ag, *gb;
 	al = new double[n*n];
 	bl = new double[n];
+	ag = new double[n*n];
+	bg = new double[n];
 	memcpy(al, a, n*n * sizeof(double));
 	memcpy(bl, b, n * sizeof(double));
+	memcpy(ag, a, n*n * sizeof(double));
+	memcpy(bg, b, n * sizeof(double));
 
 	testLapack(al, bl, n);
 	testMine(a, b, n);
+	testBlcoked(ag, bg, n);
+
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			std::cout << a[j*n + i] << " ";
+		}
+		std::cout << std::endl;
+	}
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			std::cout << ag[j*n + i] << " ";
+		}
+		std::cout << std::endl;
+	}
 
 	double sumOfSquare = 0;
 	for (int i = 0; i < n; ++i) {
 		sumOfSquare += (b[i] - bl[i])*(b[i] - bl[i]);
 	}
 	double norm = sqrt(sumOfSquare);
-	std::cout << "The norm of difference is " << std::scientific << norm << std::endl;
+	std::cout << "The norm of difference between LAPACK and My unoptimized algorithm is " << std::scientific << norm << std::endl;
+
+	sumOfSquare = 0;
+	for (int i = 0; i < n; ++i) {
+		sumOfSquare += (bg[i] - bl[i])*(bg[i] - bl[i]);
+	}
+	norm = sqrt(sumOfSquare);
+	std::cout << "The norm of difference between LAPACK and My OPTIMIZED algorithm is " << std::scientific << norm << std::endl;
 
 	delete[] a;
 	delete[] b;
